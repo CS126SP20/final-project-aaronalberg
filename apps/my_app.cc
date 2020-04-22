93edc04074dc897f2aeaa6b4fbe0de33c351742a
aaronalberg <aalberg2@illinois.edu>
2020-04-21 22:07:29 -0500

    http request example

--- apps/my_app.cc
+++ apps/my_app.cc
@@ -14,6 +14,19 @@ using cinder::app::KeyEvent;
 MyApp::MyApp() { }
 
 void MyApp::setup() {
+  try
+  {
+    // you can pass http::InternetProtocol::V6 to Request to make an IPv6 request
+    http::Request request("http://jsonplaceholder.typicode.com/todos/1");
+
+    // send a get request
+    const http::Response response = request.send("GET");
+    std::cout << std::string(response.body.begin(), response.body.end()) << '\n'; // print the result
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << "Request failed, error: " << e.what() << '\n';
+  }
 }
 
 void MyApp::update() { }
